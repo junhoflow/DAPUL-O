d05c0edca705ff13d502ff0a54308c999514e3da
Junho Kim <tikibird@naver.com>
2021-02-03 02:19:39 +0900

    SharedPreference로 todolist저장 기능 구현

--- app/src/main/java/com/example/wronganswernote/MainActivity.kt
+++ app/src/main/java/com/example/wronganswernote/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wronganswernote
 
 import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.net.Uri
@@ -9,8 +10,11 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.preference.PreferenceManager
 import android.provider.MediaStore
+import android.text.Editable
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -25,6 +29,8 @@ import java.util.*
 import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
+
+    var todo : String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -73,6 +79,22 @@ class MainActivity : AppCompatActivity() {
             email.putExtra(Intent.EXTRA_SUBJECT, "<======= ⭕다풀오⭕ 개발자에게 메일보내기 ======>")
             email.putExtra(Intent.EXTRA_TEXT, "앱 버전: ${BuildConfig.VERSION_NAME}\nAndroid(SDK): ${Build.VERSION.SDK_INT}\n 핸드폰 기종: \n 내용:")
             startActivity(email)
+        }
+        val saving_btn = findViewById<Button>(R.id.saving_btn)
+        val todolist = findViewById<EditText>(R.id.todolist )
+        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        var st = prefs.getString("todo","")
+        todolist?.setText(st)
+
+
+        saving_btn.setOnClickListener{
+            todo = todolist.text.toString()
+
+            val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+            val editor: SharedPreferences.Editor = prefs.edit()
+
+            editor.putString("todo",todo)
+            editor.apply();
         }
     }
 
