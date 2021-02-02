e05e5e6b34d2f1fa99ce35b32c4167d9828ffcf1
Junho Kim <tikibird@naver.com>
2021-02-02 23:44:56 +0900

    기본적인 레이아웃 + 갤러리에서 불러오기

--- app/src/main/java/com/example/wronganswernote/FourActivity.kt
+++ app/src/main/java/com/example/wronganswernote/FourActivity.kt
@@ -1,11 +1,86 @@
 package com.example.wronganswernote
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
+import java.lang.Exception
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FourActivity : AppCompatActivity() {
+
+    private val OPEN_GALLERY = 1
+    var checking : Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_four)
+
+        val currentDateTime = Calendar.getInstance().time
+        var dateFormat = SimpleDateFormat("yyyy년 MM월 dd일", Locale.KOREA).format(currentDateTime)
+        val nowtime = findViewById<TextView>(R.id.nowtime)
+        nowtime.text = dateFormat
+
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        imageView.setOnClickListener{
+            checking = 1
+            val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent, OPEN_GALLERY)
+        }
+        imageView2.setOnClickListener{
+            checking = 2
+            val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent, OPEN_GALLERY)
+        }
+        imageView3.setOnClickListener{
+            checking = 3
+            val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent, OPEN_GALLERY)
+        }
+        imageView4.setOnClickListener{
+            checking = 4
+            val intent: Intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setType("image/*")
+            startActivityForResult(intent, OPEN_GALLERY)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == Activity.RESULT_OK){
+            if(requestCode == OPEN_GALLERY){
+                var currentImageUrl : Uri? = data?.data
+
+                try{
+                    val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, currentImageUrl)
+                    val imageView = findViewById<ImageView>(R.id.imageView)
+                    val imageView2 = findViewById<ImageView>(R.id.imageView2)
+                    val imageView3 = findViewById<ImageView>(R.id.imageView3)
+                    val imageView4 = findViewById<ImageView>(R.id.imageView4)
+                    when(checking){
+                        1 -> imageView.setImageBitmap(bitmap)
+                        2 -> imageView2.setImageBitmap(bitmap)
+                        3 -> imageView3.setImageBitmap(bitmap)
+                        4 -> imageView4.setImageBitmap(bitmap)
+                    }
+                }catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+        } else {
+            Log.d("ActivityResult", "something wrong")
+        }
     }
 }
--- app/src/main/java/com/example/wronganswernote/MainActivity.kt
+++ app/src/main/java/com/example/wronganswernote/MainActivity.kt
@@ -25,102 +25,30 @@ import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
 
-    val REQUEST_IMAGE_CAPTURE = 1
-    lateinit var curPhotoPath: String
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setPermission()
-
-//        val button1 = findViewById<Button>(R.id.btn_camera)
-//        button1.setOnClickListener{
-//            takeCapture()
-//        }
-    }
-
-    private fun takeCapture() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also{
-                val photoFile: File? = try{
-                    createImageFile()
-                } catch (ex: IOException){
-                    null
-                }
-                photoFile?.also {
-                    val photoURI: Uri = FileProvider.getUriForFile(
-                            this,
-                            "com.example.wronganswernote.fileprovider",
-                            it
-                    )
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-                }
-            }
+        val button_two = findViewById<Button>(R.id.button_two)
+        val button_four = findViewById<Button>(R.id.button_four)
+        val button_six = findViewById<Button>(R.id.button_six)
+        val button_eight = findViewById<Button>(R.id.button_eight)
+        button_two.setOnClickListener{
+            val intent = Intent(this, TwoActivity::class.java)
+            startActivity(intent)
+        }
+        button_four.setOnClickListener{
+            val intent = Intent(this, FourActivity::class.java)
+            startActivity(intent)
+        }
+        button_six.setOnClickListener{
+            val intent = Intent(this, SixActivity::class.java)
+            startActivity(intent)
+        }
+        button_eight.setOnClickListener{
+            val intent = Intent(this, EightActivity::class.java)
+            startActivity(intent)
         }
     }
 
-    private fun createImageFile(): File? {
-        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile("JPEG_${timestamp}_",".jpg",storageDir)
-                .apply { curPhotoPath = absolutePath }
-    }
-
-    private fun setPermission() {
-        val permission = object  : PermissionListener {
-            override fun onPermissionGranted() {
-//                Toast.makeText(this@MainActivity, "권한이 허용 되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
-                Toast.makeText(this@MainActivity, "권한이 거부 되었습니다.", Toast.LENGTH_SHORT).show()
-            }
-        }
-
-        TedPermission.with(this)
-                .setPermissionListener(permission)
-                .setRationaleMessage("카메라 앱을 사용하시려면 권한을 허용해주세요.")
-                .setDeniedMessage("권한을 거부하셨습니다. [앱 설정] -> [권한] 항목에서 허용해주세요.")
-                .setPermissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
-                .check()
-    }
-
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK){
-//            val bitmap: Bitmap
-//            val file = File(curPhotoPath)
-//            val imageview = findViewById<ImageView>(R.id.imageView1)
-//            if(Build.VERSION.SDK_INT < 28){
-//                bitmap = MediaStore.Images.Media.getBitmap(contentResolver, Uri.fromFile(file))
-//                imageview.setImageBitmap(bitmap)
-//            } else {
-//                val decode = ImageDecoder.createSource(
-//                        this.contentResolver,
-//                        Uri.fromFile(file)
-//                )
-//                bitmap = ImageDecoder.decodeBitmap(decode)
-//                imageview.setImageBitmap(bitmap)
-//            }
-//            savePhoto(bitmap)
-//        }
-//    }
-
-    private fun savePhoto(bitmap: Bitmap) {
-        val folderPath = Environment.getExternalStorageDirectory().absolutePath + "/Pictures/"
-        val timestamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val fileName = "${timestamp}.jpeg"
-        val folder = File(folderPath)
-        if(!folder.isDirectory){
-            folder.mkdirs()
-        }
-
-        val out = FileOutputStream(folderPath + fileName)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out)
-        Toast.makeText(this, "오답노트가 앨범에 저장되었습니다.", Toast.LENGTH_SHORT).show()
-    }
 }
